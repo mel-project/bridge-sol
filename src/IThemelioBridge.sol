712985992347369d2e2f2c73f9dcdb18f7f4da91
sadministrator <marcothewebdev@gmail.com>
2022-10-10 15:39:33 -0500

    Update to bridge interface.

--- src/IThemelioBridge.sol
+++ src/IThemelioBridge.sol
@@ -7,34 +7,48 @@ interface IThemelioBridge {
     event BeaconUpgraded(address indexed beacon);
     event HeaderVerified(uint256 indexed height);
     event Initialized(uint8 version);
-    event StakesVerified(bytes32 keccakStakesHash, bytes32 blake3StakesHash);
+    event StakesVerified(bytes32 stakesHash);
     event TokensBurned(bytes32 indexed themelioRecipient);
-    event TokensMinted(address indexed account, uint256 indexed value);
-    event TransferBatch(address indexed operator, address indexed from, address indexed to, uint256[] ids, uint256[] values);
+    event TransferBatch(
+        address indexed operator, address indexed from, address indexed to, uint256[] ids, uint256[] values
+    );
     event TransferSingle(address indexed operator, address indexed from, address indexed to, uint256 id, uint256 value);
-    event TxVerified(bytes32 indexed tx_hash, uint256 indexed height);
+    event TxVerified(uint256 indexed height, bytes32 indexed txHash);
     event URI(string value, uint256 indexed id);
     event Upgraded(address indexed implementation);
 
-    function balanceOf(address account, uint256 id) view external returns (uint256);
-    function balanceOfBatch(address[] memory accounts, uint256[] memory ids) view external returns (uint256[] memory);
-    function burn(address account_, uint256 id_, uint256 value_, bytes32 themelioRecipient_) external;
-    function burnBatch(address account_, uint256[] memory ids_, uint256[] memory values_, bytes32 themelioRecipient_) external;
-    function headerLimbo(bytes32) view external returns (uint128 votes, uint64 bytesVerified, uint64 stakeDocIndex);
-    function headers(uint256) view external returns (bytes32 transactionsHash, bytes32 stakesHash);
+    function balanceOf(address account, uint256 id) external view returns (uint256);
+    function balanceOfBatch(address[] memory accounts, uint256[] memory ids) external view returns (uint256[] memory);
+    function burn(address account_, bytes32 txHash_, bytes32 themelioRecipient_) external;
+    function burnBatch(address account_, bytes32[] memory txHashes_, bytes32 themelioRecipient_) external;
+    function coins(bytes32) external view returns (uint256 denom, uint256 value, bytes32 status);
+    function headerLimbo(bytes32) external view returns (uint128 votes, uint64 bytesVerified, uint64 stakeDocIndex);
+    function headers(uint256) external view returns (bytes32 transactionsHash, bytes32 stakesHash);
     function initialize(uint256 blockHeight_, bytes32 transactionsHash_, bytes32 stakesHash_) external;
-    function isApprovedForAll(address account, address operator) view external returns (bool);
-    function proxiableUUID() view external returns (bytes32);
-    function safeBatchTransferFrom(address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) external;
+    function isApprovedForAll(address account, address operator) external view returns (bool);
+    function proxiableUUID() external view returns (bytes32);
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) external;
     function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes memory data) external;
     function setApprovalForAll(address operator, bool approved) external;
-    function spends(bytes32) view external returns (bool);
-    function supportsInterface(bytes4 interfaceId) view external returns (bool);
+    function stakesHashes(bytes32) external view returns (bytes32);
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
     function upgradeTo(address newImplementation) external;
-    function upgradeToAndCall(address newImplementation, bytes memory data) payable external;
-    function uri(uint256) view external returns (string memory);
-    function verifyHeader(uint256 verifierHeight_, bytes memory header_, bytes memory stakes_, bytes32[] memory signatures_, bool firstTime_) external returns (bool);
+    function upgradeToAndCall(address newImplementation, bytes memory data) external payable;
+    function uri(uint256) external view returns (string memory);
+    function verifyHeader(
+        uint256 verifierHeight_,
+        bytes memory header_,
+        bytes memory stakes_,
+        bytes32[] memory signatures_
+    ) external returns (bool);
     function verifyStakes(bytes memory stakes_) external returns (bool);
-    function verifyTx(bytes memory transaction_, uint256 txIndex_, uint256 blockHeight_, bytes32[] memory proof_) external returns (bool);
+    function verifyTx(bytes memory transaction_, uint256 txIndex_, uint256 blockHeight_, bytes32[] memory proof_)
+        external
+        returns (bool);
 }
-
